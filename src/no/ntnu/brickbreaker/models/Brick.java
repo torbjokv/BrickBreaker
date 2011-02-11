c5374d931c022e3241f373b9d0dbc986f85cd0a9
Kristoffer Eckhoff <kristoffereckhoff@gmail.com>
2011-02-11 18:48:43 +0100

    game update

---
+++ src/no/ntnu/brickbreaker/models/Brick.java
@@ -0,0 +1,16 @@
+package no.ntnu.brickbreaker.models;
+
+import org.anddev.andengine.entity.primitive.Rectangle;
+
+public class Brick extends Rectangle{
+
+	public Brick(float pX, float pY, float pWidth, float pHeight) {
+		super(pX, pY, pWidth, pHeight);
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void onManagedUpdate(final float pSecondsElapsed) {
+			this.setColor(1.0f,1.0f,1.0f);
+	}
+
+}
---
+++ src/no/ntnu/brickbreaker/models/BrickMap.java
@@ -0,0 +1,14 @@
+package no.ntnu.brickbreaker.models;
+
+public class BrickMap{
+
+
+	Brick[][] map;
+	public BrickMap(Brick[][] map) {
+		super();
+		this.map = map;
+	}
+
+
+
+}
