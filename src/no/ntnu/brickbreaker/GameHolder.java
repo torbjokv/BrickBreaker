9b9aead79201613f8dd96073f69f99bddcab08b3
Torbjørn Kvåle <torbjoernkvaale@gmail.com>
2011-02-11 02:44:55 +0100

    La til states og håndtering av de

--- src/no/ntnu/brickbreaker/GameHolder.java
+++ src/no/ntnu/brickbreaker/GameHolder.java
@@ -7,15 +7,69 @@ public class GameHolder extends Observable<GameObserver>{
 	
 	private static GameHolder instance = new GameHolder();
 	
+	private GameState gameState;
+	private GameState inGameState = new InGameState(this);
+	private GameState newGameState = new NewGameState(this);
+	private GameState pausedGameState = new PausedGameState(this);
+	
 	private GameHolder() {
 		super();
+		gameState = newGameState;
 	}
-	
+
 	public static GameHolder getInstance() {
 		if(instance == null) {
 			return new GameHolder();
 		}
 		return instance;
+	}
+
+	public void startGame() {
+		gameState.startGame();
+	}
+
+	public void pauseGame() {
+		gameState.pauseGame();
+	}
+	
+	public void resumeGame() {
+		gameState.resumeGame();
+	}
+
+	public void killGame() {
+		gameState.killGame();
+	}
+
+	public void setGameState(GameState gameState) {
+		this.gameState = gameState;
+	}
+
+	public GameState getGameState() {
+		return gameState;
+	}
+	
+	public GameState getNewGameState() {
+		return newGameState;
+	}
+
+	public void setNewGameState(GameState newGameState) {
+		this.newGameState = newGameState;
+	}
+
+	public GameState getPausedGameState() {
+		return pausedGameState;
+	}
+
+	public void setPausedGameState(GameState pausedGameState) {
+		this.pausedGameState = pausedGameState;
+	}
+
+	public void setInGameState(GameState inGameState) {
+		this.inGameState = inGameState;
+	}
+
+	public GameState getInGameState() {
+		return inGameState;
 	}
 	
 	
---
+++ src/no/ntnu/brickbreaker/NewGameState.java
@@ -0,0 +1,35 @@
+package no.ntnu.brickbreaker;
+
+public class NewGameState implements GameState {
+
+	private GameHolder gameHolder;
+
+	public NewGameState(GameHolder gameHolder) {
+		this.gameHolder = gameHolder;
+	}
+
+	@Override
+	public void startGame() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pauseGame() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resumeGame() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void killGame() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
